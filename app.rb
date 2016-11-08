0fcd6dc923343d2e031db68720381b9a88725776
Jonas Demeulemeester <jonasdemeulemeester@hotmail.be>
2016-11-08 16:06:33 +0100

    Create basic chat app

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'rubygems'
 require 'mqtt'
 
-# Publish example
-MQTT::Client.connect('test.mosquitto.org') do |c|
-  c.publish('test', 'message')
-end
+puts "Input name please."
+@name = gets.chomp
 
-# Subscribe example
-MQTT::Client.connect('test.mosquitto.org') do |c|
-  # If you pass a block to the get method, then it will loop
-  c.get('test') do |topic,message|
-    puts "#{topic}: #{message}"
+Thread.new do
+  # Publish example
+  puts "enter message"
+  @text = gets.chomp
+  MQTT::Client.connect('10.177.33.144') do |c|
+    c.publish('BABL', @name+ ': ' + @text)
   end
 end
+  # Subscribe example
+  MQTT::Client.connect('10.177.33.144') do |c|
+    # If you pass a block to the get method, then it will loop
+    c.get('BABL') do |topic,message|
+      puts "#{topic}: #{message}"
+    end
+  end
